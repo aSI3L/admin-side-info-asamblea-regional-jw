9cbc873d7ffd230843805ed1c12d8122227f8360
Asiel Alvarez <asiel.alvarez@alumnos.frm.utn.edu.ar>
2025-06-10 13:10:15 -0300

    Fix header colors

--- src/components/common/AppHeader/AppHeader.tsx
+++ src/components/common/AppHeader/AppHeader.tsx
@@ -4,7 +4,7 @@ import { Separator } from "@/src/components/ui/separator";
 
 export function AppHeader() {
   return (
-    <header className="flex h-16 items-center justify-between px-4 py-2 bg-gray-800 text-white sticky top-0 z-10 rounded-2xl">
+    <header className="flex h-16 items-center justify-between px-4 py-2 text-white sticky top-0 z-10">
         <div className="flex items-center gap-1">
             <SidebarTrigger />
             <Separator
