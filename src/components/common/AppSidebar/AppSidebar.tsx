beb35086640bc378e27c64f354303d80af5f013c
Asiel Alvarez <asiel.alvarez@alumnos.frm.utn.edu.ar>
2025-06-10 00:37:48 -0300

    Fix inset sidebar

--- src/components/common/AppSidebar/AppSidebar.tsx
+++ src/components/common/AppSidebar/AppSidebar.tsx
@@ -11,28 +11,27 @@ import { Button } from "@/src/components/ui/button";
 export function AppSidebar() {
   const { toggleSidebar } = useSidebar()
   return (
-    <aside>
-      <Sidebar variant="inset">
-        <Button variant="ghost" onClick={toggleSidebar} className="absolute z-1 right-0.5 top-0.5 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary md:hidden">
+    <Sidebar variant="inset">
+      <Button variant="ghost" onClick={toggleSidebar} className="absolute z-1 right-0.5 top-0.5 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary md:hidden">
           <X className="w-4 h-4" />
           <span className="sr-only">Cerrar menú</span>
-        </Button>
-        <SidebarHeader className="px-5 pt-5">
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <Link href="/" className="flex items-center gap-2">
-                <Avatar className="w-6 h-6 md:w-7 md:h-7">
-                  <AvatarFallback>
-                    <InfoCircle className="w-full h-full" />
-                  </AvatarFallback>
-                </Avatar>
-                <span className="font-bold text-md md:text-lg">Panel de Informes</span>  
-              </Link>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarHeader>
-        <SidebarContent className="px-2">
-          <SidebarGroup>
+      </Button>
+      <SidebarHeader className="px-5 pt-5">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <Link href="/" className="flex items-center gap-2">
+              <Avatar className="w-6 h-6 md:w-7 md:h-7">
+                <AvatarFallback>
+                  <InfoCircle className="w-full h-full" />
+                </AvatarFallback>
+              </Avatar>
+              <span className="font-bold text-md md:text-lg">Panel de Informes</span>  
+            </Link>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent className="px-2">
+        <SidebarGroup>
             <SidebarGroupContent>
               <SidebarMenu>
                 {
@@ -49,9 +48,8 @@ export function AppSidebar() {
                 }
               </SidebarMenu>
             </SidebarGroupContent>
-          </SidebarGroup>
-        </SidebarContent>
-      </Sidebar>
-    </aside>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
   );
 }
