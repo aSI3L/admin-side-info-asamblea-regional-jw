25975af5b71f361ee42026a4aca1b7e0680d06fc
Asiel Alvarez <asiel.alvarez@alumnos.frm.utn.edu.ar>
2025-07-22 11:25:43 -0300

    Show auth user data

--- src/components/common/AppHeader/components/DropdownAvatar.tsx
+++ src/components/common/AppHeader/components/DropdownAvatar.tsx
@@ -1,16 +1,17 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { useAuth } from "@/hooks/useAuth";
 import { LogOut } from "lucide-react";
 
 export function DropdownAvatar() {
+    const { authUser, logout } = useAuth()
   return (
     <DropdownMenu>
         <DropdownMenuTrigger asChild>
             <Button variant="ghost" size="icon" className="cursor-pointer">
                 <Avatar className="h-8 w-8 rounded-lg">
-                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"/>
-                    <AvatarFallback>CN</AvatarFallback>
+                    <AvatarImage src={authUser?.photoURL || "no-profile-icon.png"} alt="Imagen Avatar"/>
                 </Avatar>
             </Button>
         </DropdownMenuTrigger>
@@ -22,17 +23,16 @@ export function DropdownAvatar() {
             <DropdownMenuLabel className="p-0 font-normal">
                 <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                     <Avatar className="h-8 w-8 rounded-lg">
-                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                        <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                        <AvatarImage src={authUser?.photoURL || "no-profile-icon.png"} alt="Imagen Avatar" />
                     </Avatar>
                     <div className="grid flex-1 text-left text-sm leading-tight">
-                        <span className="truncate font-medium">@shadcn</span>
-                        <span className="truncate text-xs">m@gmail.com</span>
+                        <span className="truncate font-medium">{authUser?.displayName}</span>
+                        <span className="truncate text-xs">{authUser?.email}</span>
                     </div>
                 </div>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem className="cursor-pointer">
+            <DropdownMenuItem className="cursor-pointer" onClick={logout}>
               <LogOut />
               Log out
             </DropdownMenuItem>
